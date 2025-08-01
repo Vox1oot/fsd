fe63491f9cf285b01a83f3838bb216b7b28902c4
Vitaliy Morzhov <voxfoot@gmail.com>
2025-08-01 19:34:59 +0500

    Add Makefile and update dependencies for React Router integration

---
+++ Makefile
@@ -0,0 +1,11 @@
+dev:
+		bun dev
+
+build:
+		bun run build
+
+start:
+	bun run preview
+
+lint:
+	bun run lint
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: () => <div>Hllo</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div>Hello FSD</div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
